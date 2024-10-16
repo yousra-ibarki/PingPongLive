453bf8e9c8b2539fc779e32c2fbb6c2a00a0b087
Ahmed Er-raoui <aer-raou@e1r4p1.1337.ma>
2024-10-16 14:52:03 +0100

    feat: add sum styling to register page and implement it as a popup for login page

--- frontend/app/chat/page.tsx
+++ frontend/app/chat/page.tsx
@@ -1,26 +1,3 @@
-// "use client";
-
-// import { useRouter } from 'next/router';
-// import ChatApp from './ChatApp';
-// import '../globals.css';
-
-// const ChatPage = () => {
-//   const router = useRouter();
-//   const { username } = router.query; // Get username from URL
-
-//   return (
-//     <div className="lg:m-10 p-2 rounded-tr-lg bg-[#393E46] border border-[#FFD369] rounded-lg max-h-full">
-//       {username ? (
-//         <ChatApp username={username} />
-//       ) : (
-//         <p>Please enter a username in the URL: ?username=YourName</p>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default ChatPage;
-
 "use client";
 
 import React, { useEffect, useState } from 'react';
@@ -28,24 +5,11 @@ import ChatApp from './ChatApp';
 import '../globals.css';
 
 function App() {
-    // const [username, setUsername] = useState(null);
-    const [username, setUsername] = useState<string | null>(null);
-
-    useEffect(() => {
-        // Access URL parameters after component mounts
-        const searchParams = new URLSearchParams(window.location.search);
-        const usernameParam = searchParams.get('username');
-        setUsername(usernameParam);
-    }, []);
 
     return (
         <div className="lg:m-10 p-2 rounded-tr-lg bg-[#393E46] border border-[#FFD369] rounded-lg max-h-full">
-            {username ? (
-                <ChatApp username={username} />
-            ) : (
-                <p>Please enter a username in the URL: ?username=YourName</p>
-            )}
-        </div>
+                <ChatApp/>
+         </div>
     );
 }
 
