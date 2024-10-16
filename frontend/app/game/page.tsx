7aaa699f21524b8c414772e8b48198ba9c45c422
Yousra Ibarki <yousra.ibarki@gmail.com>
2024-10-16 16:21:45 +0100

    front game added

---
+++ frontend/app/game/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../globals.css';
+import {GameHome} from './Game'
+
+function App() {
+    return (
+        <>
+            <GameHome /> 
+        </>
+    )
+}
+
+export default App;
