57cb861b792ca5815ad638c00e9be0d59cf2139c
Abdelfattah Berkach <abberkac@e1r2p5.1337.ma>
2024-12-10 18:50:21 +0100

    almost fix the profile users and invitations

--- frontend/app/Components/profile.js
+++ frontend/app/Components/profile.js
@@ -1,17 +1,17 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../globals.css";
 import Axios from "./axios";
-import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
-import GameData from "../userProfile/[userId]/(profileComponents)/gameData";
-// import {
-//   sendFriendRequest,
-//   friendshipStatus,
-//   blockUser,
-//   unblockUser,
-// } from "../userProfile/[userId]/(profileComponents)/profileFunctions";
+import GameData from "../user-profile/[userId]/(profileComponents)/gameData";
+import {
+  friendshipStatusFunc,
+  sendFriendRequest,
+  blockUser,
+  unblockUser,
+  removeFriendship,
+} from "../user-profile/[userId]/(profileComponents)/profileFunctions";
 
 const Profile = ({ userData, myProfile }) => {
   const userId = userData.id;
@@ -30,13 +30,10 @@ const Profile = ({ userData, myProfile }) => {
         const friendshipResponse = await Axios.get(
           `/api/friends/friendship_status/${userId}/`
         );
-
-        const response = await Axios.get(`/api/friends/friend_requests/`);
-        console.log("FRIEND REQUESTS", response.data);
         setFriendshipStatus(friendshipResponse.data);
       } catch (err) {
         setError(err.response?.data?.message || "An error occurred");
-        console.log(err);
+        toast.error(error);
       } finally {
         setLoading(false);
       }
@@ -44,161 +41,41 @@ const Profile = ({ userData, myProfile }) => {
     fetchUserProfile();
   }, []);
 
-  // friendship status -----------------------------------------------------------
-
-  const friendshipStatusFunc = async (userId) => {
-    try {
-      // await Axios.get(`/api/friends/friendship_status/${userId}/`);
-      const response = await Axios.get(
-        `/api/friends/friendship_status/${userId}/`
-      );
-      setFriendshipStatus(response.data);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  // send friend request ---------------------------------------------------------
-  const sendFriendRequest = async (userId) => {
-    if (String(userId) === String(currentUserId)) {
-      toast.error("Cannot send friend request to yourself");
-      return;
-    }
-    console;
-    // console.log('FRIENDSHIP STATUS', FriendshipStatu.can_send_request);
-    if (friendshipStatus.can_send_request === true) {
-      try {
-        const response = await Axios.post(
-          `/api/friends/send_friend_request/${userId}/`
-        );
-        await friendshipStatusFunc(userId);
-        toast.success("Friend request sent successfully");
-      } catch (err) {
-        if (err.response?.data?.error) {
-          toast.error(err.response.data.error);
-        }
-      }
-    } else {
-      // console.log(FriendshipStatu.can_send_request);
-      toast.error("Cannot send friend request");
-    }
-  };
-
-  // friend requests -------------------------------------------------------------
-
-  // const friendRequests = async (userId) => {
-  //   try {
-  //     const response = await Axios.get(`/api/friends/friend_requests/`);
-  //     console.log("FRIEND REQUESTS", response.data);
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // };
-
-  // block user ------------------------------------------------------------------
-
-  // const blockUser = async (userId) => {
-  //   console.log("CURRENT USER ID", currentUserId);
-  //   console.log("USER ID", userId);
-  //   if (String(userId) === String(currentUserId)) {
-  //     toast.error("Cannot block yourself");
-  //     return;
-  //   }
-  //   try {
-  //     // Check if user is already blocked
-  //     if (friendshipStatus?.is_blocked) {
-  //       toast.error("User is already blocked");
-  //       return;
-  //     }
-
-  //     const response = await Axios.post(`/api/friends/block_user/${userId}/`);
-  //     console.log(response.data);
-  //     await friendshipStatusFunc(userId);
-  //     toast.success("User blocked successfully");
-  //   } catch (err) {
-  //     // If we get a 400 error but it's because the user is already blocked,
-  //     // we can still show a success message
-  //     if (
-  //       err.response?.status === 400 &&
-  //       err.response?.data?.error === "User is already blocked"
-  //     ) {
-  //       await friendshipStatusFunc(userId);
-  //       toast.success("User is blocked");
-  //     } else if (err.response?.data?.error) {
-  //       toast.error(err.response.data.error);
-  //     }
-  //   }
-  // };
-
-  // // unblock user ---------------------------------------------------------------
-
-  // const unblockUser = async (userId) => {
-  //   console.log("CURRENT USER ID", currentUserId);
-  //   console.log("USER ID", userId);
-  //   if (String(userId) === String(currentUserId)) {
-  //     toast.error("Cannot unblock yourself");
-  //     return;
-  //   }
-  //   if (friendshipStatus?.is_blocked === true) {
-  //     try {
-  //       await Axios.post(`/api/friends/unblock_user/${userId}/`);
-  //       await friendshipStatusFunc(userId);
-  //       toast.success("User unblocked successfully");
-  //     } catch (err) {
-  //       if (err.response?.data?.error) {
-  //         toast.error(err.response.data.error);
-  //       }
-  //     }
-  //   } else {
-  //     toast.error("User is not blocked");
-  //   }
-  // };
-
-  // //remove friendship -----------------------------------------------------------
-
-  // const removeFriendship = async (userId) => {
-  //   if (friendshipStatus.friendship_status === null) {
-  //     toast.error("No friendship to remove");
-  //     return;
-  //   }
-  //   try {
-  //     const response = await Axios.delete(
-  //       `/api/friends/remove_friendship/${userId}/`
-  //     );
-  //     console.log(response.data);
-  //     await friendshipStatusFunc(userId);
-  //     toast.success("Friendship removed successfully");
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // };
-
-  //-------------------------------------------------------------------------------
-
-  // if (error) {
-  //   toast.error(error);
-  // }
   if (Loading) {
     return (
-      <div className="h-[100px] flex items-center justify-center m-2  fade-in-globale">
+      <div className="h-[100px] flex items-center justify-center m-2 fade-in-globale">
         <div className="h-[60px] w-[60px] loader"></div>
       </div>
     );
   }
 
   const levelPercentage = (userData.level - Math.floor(userData.level)) * 100;
+  console.log("friendshipStatus", friendshipStatus);
   return (
-    <div className="h-[1200px] flex flex-col m-2 bg-[#131313] fade-in-globale rounded-xl border border-[#FFD369] ">
+    <div className="h-[1100px] flex flex-col m-2 bg-[#131313] fade-in-globale rounded-xl border border-[#FFD369] ">
       <div className="h-[30%] flex flex-col">
-        <div className=" w-full flex flex-col items-center justify-center m-4">
+        <div className="w-full flex flex-col items-center justify-center m-4">
+          {!myProfile && (
+            <div className="relative">
+              {/* Online status dot */}
+              <div className="w-[130px] h-[130px] absolute">
+                <div
+                  className={`absolute w-5 h-5 rounded-full right-20 bottom-1 bg-${
+                    userData.is_online ? "green" : "red"
+                  }-500 border border-[#FFD369]`}
+                ></div>
+              </div>
+            </div>
+          )}
           <img
             src={userData.profileImage || "../user_img.svg"}
             alt="user_img"
             width="130"
             height="130"
-            className=" rounded-full"
+            className="rounded-full border-2 border-[#FFD369]"
           />
-          <div className="text-base m-4 text-yellow-700 dark:text-[#FFD369]">
+
+          <div className="m-2 text-lg dark:text-[#FFD369]">
             {userData.username}
           </div>
         </div>
@@ -219,44 +96,54 @@ const Profile = ({ userData, myProfile }) => {
 
       <GameData userData={userData} />
       {!myProfile && (
-        <div className="flex items-center text-black text-center  justify-evenly">
+        <div className="flex items-center text-black text-center justify-evenly h-[10%]">
           <button
             className="bg-[#00D1FF] m-2 p-2 h-[50px] w-[150px] rounded-lg"
-            onClick={() => {
-              sendFriendRequest(userId);
-            }}
+            onClick={() =>
+              sendFriendRequest(
+                userId,
+                currentUserId,
+                friendshipStatus,
+                setFriendshipStatus
+              )
+            }
           >
             Send Friend Request
           </button>
-          {/* <button
-            className="bg-blue-500 m-2 p-2 h-[50px] w-[150px] rounded-lg"
-            onClick={() => {
-              friendshipStatus(userId);
-            }}
-          >
-            Friendship Status
-          </button>
-
           <button
             className="bg-[#FF0000] m-2 p-2 h-[50px] w-[150px] rounded-lg"
-            onClick={() => {
-              blockUser(userId);
-              if (friendshipStatus.is_blocked) {
-                toast.error("User is already blocked");
-              }
-            }}
+            onClick={() =>
+              blockUser(
+                userId,
+                currentUserId,
+                friendshipStatus,
+                setFriendshipStatus
+              )
+            }
           >
             Block User
           </button>
-         
           <button
             className="bg-blue-500 m-2 p-2 h-[50px] w-[150px] rounded-lg"
-            onClick={() => {
-              unblockUser(userId);
-            }}
+            onClick={() =>
+              unblockUser(
+                userId,
+                currentUserId,
+                friendshipStatus,
+                setFriendshipStatus
+              )
+            }
           >
             Unblock User
-          </button> */}
+          </button>
+          <button
+            className="bg-[#FF6347] m-2 p-2 h-[50px] w-[150px] rounded-lg"
+            onClick={() =>
+              removeFriendship(userId, friendshipStatus, setFriendshipStatus)
+            }
+          >
+            Remove Friendship
+          </button>
         </div>
       )}
     </div>
@@ -264,40 +151,3 @@ const Profile = ({ userData, myProfile }) => {
 };
 
 export default Profile;
-
-// ---------------------------------- friend requests ----------------------------------
-{
-  /* <div className="w-full md:w-[25%] h-full md:h-[80%] mt-4 flex flex-col items-center text-white text-center p-2 px-4 border-2 border-[#393E46] rounded-lg overflow-y-auto scrollbar-thin scrollbar-thumb-[#FFD369] scrollbar-track-gray-800">
-  <div className="text-white text-center font-kreon text-2xl mb-2">
-    Friend Requests
-  </div>
-  {friendRequests.length === 0 ? (
-    <div className="text-gray-400">No pending friend requests</div>
-  ) : (
-    friendRequests.map((request) => (
-      <div
-        key={request.id}
-        className="bg-[#393E46] m-1 mt-2 p-3 w-full rounded-lg"
-      >
-        <div className="text-[#FFD369] font-kreon text-lg mb-2">
-          {request.from_user.username}
-        </div>
-        <div className="flex justify-center gap-2">
-          <button
-            onClick={() => handleFriendRequest(request.id, "accept")}
-            className="bg-green-600 hover:bg-green-700 text-white px-3 py-1 rounded"
-          >
-            Accept
-          </button>
-          <button
-            onClick={() => handleFriendRequest(request.id, "reject")}
-            className="bg-red-600 hover:bg-red-700 text-white px-3 py-1 rounded"
-          >
-            Reject
-          </button>
-        </div>
-      </div>
-    ))
-  )}
-</div> */
-}
