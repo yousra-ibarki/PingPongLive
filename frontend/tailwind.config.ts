e4cd576adabe42d97726445f5d2519a424b6465a
aer-raou <aer-raou@student.1337.ma>
2024-12-25 16:08:08 +0100

    remove last_active user field update on each WebSocket request to improve performance of game

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,104 +1,72 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    darkMode: ["class"],
-    content: [
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	extend: {
-  		colors: {
-  			golden: '#FFD369',
-  			blue_dark: '#222831',
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		},
-  		keyframes: {
-  			fadeIn: {
-  				'0%': {
-  					opacity: '0'
-  				},
-  				'100%': {
-  					opacity: '1'
-  				}
-  			}
-  		},
-  		animation: {
-  			fadeIn: 'fadeIn 2s ease-in forwards'
-  		},
-  		screens: {
-  			phone: '700px',
-  			tablet: '1247px',
-  			laptop: '1400px',
-  			desktop: '1500px',
-  			custom1: '1660px',
-  			'h-sm': {
-  				raw: '(min-height: 0px)'
-  			},
-  			'h-md': {
-  				raw: '(min-height: 700px)'
-  			},
-  			'h-lg': {
-  				raw: '(min-height: 900px)'
-  			},
-  			'h-xl': {
-  				raw: '(min-height: 1100px)'
-  			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	},
-  	screens: {
-  		sm: '640px',
-  		md: '768px',
-  		lg: '1024px',
-  		xl: '1280px',
-  		'2xl': '1536px'
-  	}
+    extend: {
+      colors: {
+        golden: '#FFD369',
+        blue_dark: '#222831',
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' }, // Convert number to string
+          '100%': { opacity: '1' }, // Convert number to string
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 2s ease-in forwards',
+      },
+      screens: {
+        'phone': '700px',
+        // => @media (min-width: 640px) { ... }
+
+        'tablet': '1247px',
+        // => @media (min-width: 640px) { ... }
+  
+        'laptop': '1400px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'desktop': '1500px',
+        // => @media (min-width: 1280px) { ... }
+
+        'custom1': '1660px',
+        // => @media (min-width: 1280px) { ... }
+
+         // Custom height-based breakpoints
+
+         'h-sm': { raw: '(min-height: 0px)' },  // For small screens with min-height of 500px
+         
+         'h-md': { raw: '(min-height: 700px)' },  // For medium screens with min-height of 700px
+         
+         'h-lg': { raw: '(min-height: 900px)' },  // For large screens with min-height of 900px
+         
+         'h-xl': { raw: '(min-height: 1100px)' }, // For extra-large screens with min-height of 1100px
+      },
+    },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [],
 };
 export default config;
