70254088c69b802cc8405601c252c51f1f450867
Abdelfattah Berkach <abberkac@e3r10p2.1337.ma>
2025-01-09 18:10:39 +0100

    .

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,4 +1,4 @@
-zfrom django.shortcuts import render, redirect
+from django.shortcuts import render, redirect
 from rest_framework.views import APIView
 from rest_framework.response import Response
 import requests
